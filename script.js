d3fd37359cb1fabfa7f7444f24e9f57481dde0e0
MdRasel2000 <ctgmdraselislam@gmail.com>
2025-02-01 17:31:00 +0600

    js event & DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function sum() {
+
+event.preventDefault();
+    
+let x = document.getElementById("number1").value;
+let y = document.getElementById("number2").value;
+
+let result =parseFloat(x)+parseFloat(y);
+
+document.getElementById("result").innerHTML= "The result is = "+ result;
+}
